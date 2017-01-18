6c3be960f43a53039a0948e324bf311914060141
fabianmikaelsson <fabian.mikaelsson@hotmail.com>
2017-01-18 15:11:45 +0100

    First commit

---
+++ labb2_FM/labb2_FM/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace labb2_FM
+{
+	[Activity(Label = "labb2_FM", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
+
